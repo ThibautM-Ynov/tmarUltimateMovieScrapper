7d88020efed3043866f4c5ea0379e628ffab4d06
Thibaut MARTIN <thibaut.martin@insign.fr>
2018-12-07 12:35:42 +0100

    Remove Tabs from imports and use them with lazyloader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import {TabsFooterPage} from "../pages/tabs-footer/tabs-footer";
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsFooterPage;
+  rootPage:any = 'TabsFooterPage';
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
